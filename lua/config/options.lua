224adf53fc984a5fef6ac9b3eabd1bdc51d8ae83
malgorzatatalaga <malgorzatatalaga32@gmail.com>
2025-08-24 22:16:37 +0200

    options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+opt.relativenumber = true
+opt.number = true
+
+-- tabs and indentation
+opt.autoindent = true
+
+opt.wrap = true
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true

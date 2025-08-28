0c60519897e35b389a64669fbba5982fb7d3f9a5
malgorzatatalaga <malgorzatatalaga32@gmail.com>
2025-08-28 22:07:10 +0200

    refactoring in progress

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- window management
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
 local opt = vim.opt
 opt.relativenumber = true
 opt.number = true

bd39219f5fec7b017b43dc91500a2c9911ed3d2e
malgorzatatalaga <malgorzatatalaga32@gmail.com>
2025-08-29 18:31:55 +0200

    reset

--- lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " "
-
-local keymap = vim.keymap
-
--- window management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })

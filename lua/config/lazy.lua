3d07ce0a51ffbdb2f38436057d1afd93bb4b1207
malgorzatatalaga <malgorzatatalaga32@gmail.com>
2025-09-10 23:10:26 +0200

    lsp config and mason

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
 		-- add LazyVim and import its plugins
 		-- { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		-- import/override with your plugins
-		{ import = "config.plugins" },
+		{{import = "plugins"},{import = 'plugins.lsp'}}
 	},
 	defaults = {
 		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -34,7 +34,7 @@ require("lazy").setup({
 		version = false, -- always use the latest git commit
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
-	install = { colorscheme = { "tokyonight", "habamax" } },
+	-- install = { colorscheme = { "tokyonight", "habamax" } },
 	checker = {
 		enabled = true, -- check for plugin updates periodically
 		notify = false, -- notify on update
@@ -55,3 +55,4 @@ require("lazy").setup({
 		},
 	},
 })
+
---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,31 @@
+return {
+	"mason-org/mason.nvim",
+	dependencies = {
+		"mason-org/mason-lspconfig.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+            				package_pending = "➜",
+            				package_uninstalled = "✗"
+				}
+			}
+		})
+
+		mason_lspconfig.setup({
+			ensure_installed = { 
+				"lua_ls",
+				"rust_analyzer",
+				"pyright",
+				"gopls"
+			},
+			
+			automatic_installation = true,
+		})
+	end,
+}
